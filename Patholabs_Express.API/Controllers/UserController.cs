1b9a66dd7812bc1b22df4368e1439f9e20558323
Apoorv2010 <apoorvjain6@gmail.com>
2021-06-17 10:40:11 +0530

    Add project files.

---
+++ Patholabs_Express.API/Controllers/UserController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Patholabs_Express.BuisnessLogic.DTOs;
+using Patholabs_Express.BuisnessLogic.Services;
+using Patholabs_Express.DataAccess.Entities;
+using System.Web.Http.Cors;
+
+namespace Patholabs_Express.API.Controllers
+{
+    //[EnableCors(origins: "http://www.example.com", headers: "*", methods: "*")]
+    //[DisableCors]
+    public class UserController : ApiController
+    {
+        private readonly UserService userService;
+        private readonly User_ApplicationService userAppService;
+
+        public UserController()
+        {
+            userService = new UserService();
+            userAppService = new User_ApplicationService();
+        }
+        [HttpPost]
+        public IHttpActionResult Add([FromBody] UserDto obj)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            else
+            {
+               if( userService.Add(obj))
+                {
+                    userAppService.Add(obj);
+                    return Ok(new Responce() { Success = true, Message = "User Registered Successfully" });
+                }
+               else
+                return Ok(new Responce() { Success = false, Message = "User Registered Successfully" });
+
+
+
+            }
+
+        }
+
+
+    }
+}
---
+++ Patholabs_Express.BuisnessLogic/Services/User_ApplicationService.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Patholabs_Express.BuisnessLogic.DTOs;
+using Patholabs_Express.DataAccess;
+using Patholabs_Express.DataAccess.Entities;
+using Patholabs_Express.DataAccess.Repository;
+
+namespace Patholabs_Express.BuisnessLogic.Services
+{
+    public class User_ApplicationService
+    {
+        private readonly Application_UserRepository userRepository;
+        private readonly UserRepository userRepositoryforuser;
+
+
+
+        private readonly Patholabs_ExpressModel context;
+        public User_ApplicationService()
+        {
+            context = new Patholabs_ExpressModel();
+            userRepository = new Application_UserRepository();
+            userRepositoryforuser = new UserRepository();
+        }
+
+
+        public void Dispose()
+        {
+            context.Dispose();
+        }
+        public List<Application_User> GetAll()
+        {
+            try
+            {
+                var user = from i in context.application_Users
+                           select i;
+                return user.ToList();
+            }
+
+            catch (System.Data.Common.DbException ex)
+            {
+                throw new Patholabs_ExpressException("Error reading data", ex);
+            }
+
+            catch (Exception ex)
+            {
+                throw new Patholabs_ExpressException("Unknown error while reading User Admin data", ex);
+            }
+
+        }
+
+
+        public bool Add(UserDto dto)
+        {
+            try
+            {
+                if (!userRepository.Exists(dto.Email))
+                {
+                    dto.UserId = userRepositoryforuser.GetUserId(dto.Email);
+                    var user = new Application_User { UserId = dto.UserId, Email = dto.Email, UserType = dto.UserType, Password = dto.Password };
+                    return userRepository.Add(user) == 1;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (System.Data.Common.DbException ex)
+            {
+                throw new Patholabs_ExpressException("Error reading data", ex);
+            }
+
+            catch (Exception ex)
+            {
+                throw new Patholabs_ExpressException("Unknown error while reading User Admin data", ex);
+            }
+
+        }
+
+
+
+
+        public bool Authenticate(string email, string password, enUserType userType)
+        {
+            try
+            {
+                bool Succeded = userRepository.ValidateCredentials(email, password, userType);
+                if (Succeded)
+                {
+                    return Succeded;
+                }
+
+                return false;
+            }
+            catch (System.Data.Common.DbException ex)
+            {
+                throw new Patholabs_ExpressException("Error reading data", ex);
+            }
+
+            catch (Exception ex)
+            {
+                throw new Patholabs_ExpressException("Unknown error while reading User Admin data", ex);
+            }
+        }
+    }
+}
